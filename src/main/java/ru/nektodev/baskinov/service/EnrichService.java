57b378445c9ed76ed4ef08729f6aaf26d8cfd650
nektodev <ts.slawa@gmail.com>
2016-07-08 20:47:31 +0300

    Add EnrichService

---
+++ src/main/java/ru/nektodev/baskinov/service/EnrichService.java
@@ -0,0 +1,7 @@
+package ru.nektodev.baskinov.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface EnrichService {
+}
---
+++ src/main/java/ru/nektodev/baskinov/service/impl/EnrichServiceImpl.java
@@ -0,0 +1,8 @@
+package ru.nektodev.baskinov.service.impl;
+
+import org.springframework.stereotype.Service;
+import ru.nektodev.baskinov.service.EnrichService;
+
+@Service
+public class EnrichServiceImpl implements EnrichService {
+}
