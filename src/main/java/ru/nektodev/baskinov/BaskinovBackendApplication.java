f11d5ca6e15235c80f555dc74fddcd7d92b37351
nektodev <ts.slawa@gmail.com>
2016-07-16 22:57:01 +0300

    Add /api/ prefix

--- src/main/java/ru/nektodev/baskinov/BaskinovBackendApplication.java
+++ src/main/java/ru/nektodev/baskinov/BaskinovBackendApplication.java
@@ -2,8 +2,11 @@ package ru.nektodev.baskinov;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -14,6 +17,19 @@ public class BaskinovBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BaskinovBackendApplication.class, args);
+	}
+
+	@Bean
+	public DispatcherServlet dispatcherServlet() {
+		return new DispatcherServlet();
+	}
+
+	@Bean
+	public ServletRegistrationBean dispatcherServletRegistration() {
+		ServletRegistrationBean registration = new ServletRegistrationBean(
+				dispatcherServlet(), "/api/*");
+		registration.setName(DispatcherServletAutoConfiguration.DEFAULT_DISPATCHER_SERVLET_REGISTRATION_BEAN_NAME);
+		return registration;
 	}
 
 	@Bean
